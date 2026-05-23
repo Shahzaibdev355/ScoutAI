c7f6719b2b575f6389127318cb72b7013bf66674
Shahzaib <shahzaibsheikh355@gmail.com>
2026-05-23 20:13:32 +0500

    new change in playwright

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ["playwright-core"],
+  // serverExternalPackages: ["playwright-core"],
+  serverExternalPackages: ["playwright"],
 };
 
 export default nextConfig;
